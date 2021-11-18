f7a095ce8e9773b5799e38ddc2934dd90883e305
Juan Martinez <78063884+mmjamm89@users.noreply.github.com>
2021-11-18 17:28:27 -0300

    calculator structure

--- src/App.js
+++ src/App.js
@@ -2,8 +2,29 @@ import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Javascript Calculator</h1>
+    <div className="App">      
+      <div className="container">
+        <div className="grid">
+          <div className="display"></div>
+          <div className="button AC">AC</div>
+          <div className="button div">/</div>
+          <div className="button times">x</div>
+          <div className="button seven">7</div>
+          <div className="button eight">8</div>
+          <div className="button nine">9</div>
+          <div className="button minus">-</div>
+          <div className="button four">4</div>
+          <div className="button five">5</div>
+          <div className="button six">6</div>
+          <div className="button plus">+</div>
+          <div className="button one">1</div>
+          <div className="button two">2</div>
+          <div className="button three">3</div>
+          <div className="button equal">=</div>
+          <div className="button zero">0</div>
+          <div className="button dot">.</div>          
+        </div>
+      </div>
     </div>
   );
 }
