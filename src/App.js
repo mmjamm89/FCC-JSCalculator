e21dcc579479285de2b41fc6bc1efcc5ab990921
Juan Martinez <78063884+mmjamm89@users.noreply.github.com>
2021-11-18 15:55:39 -0300

    setting canvas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import './style.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Javascript Calculator</h1>    
+      <h1>Javascript Calculator</h1>
     </div>
   );
 }
